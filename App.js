adbd95c467d4c8494194c02e1d70abb442d16092
Watson Chen <wc.watsonchen@gmail.com>
2022-08-11 23:39:01 -0400

    kicked off coin tracker project

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import ListItem from "./components/ListItem";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.titleWrapper}>
+        <Text style={styles.largeTitle}>Markets</Text>
+      </View>
+      <View style={styles.divider} />
+      <ListItem></ListItem>
     </View>
   );
 }
@@ -13,8 +17,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  titleWrapper: {
+    marginTop: 80,
+    paddingHorizontal: 60,
+  },
+  largeTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  divider: {
+    height: StyleSheet.hairlineWidth,
+    backgroundColor: "#A9ABB1",
+    marginHorizontal: 16,
+    marginTop: 16,
   },
 });
---
+++ components/ListItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const ListItem = () => {
+  return (
+    <View>
+      <Text>List Item</Text>
+    </View>
+  );
+};
+
+export default ListItem;
